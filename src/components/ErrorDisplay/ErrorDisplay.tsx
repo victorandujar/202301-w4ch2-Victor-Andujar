ecf0adea1a62423376ca8c4ee0761cb655fa8772
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-01-31 20:49:05 +0100

    Create error display component

---
+++ src/components/ErrorDisplay/ErrorDisplay.tsx
@@ -0,0 +1,11 @@
+const ErrorDisplay = () => {
+  return (
+    <div className="row">
+      <p className="error text-danger col">
+        Error: I couldn't retrieve anything funny
+      </p>
+    </div>
+  );
+};
+
+export default ErrorDisplay;
